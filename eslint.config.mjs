36eee511d1e1c653f3983fd331399f65aa08a480
kevin raydafa <kevinroy201104@gmail.com>
2025-10-20 16:01:11 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import daStyle from 'eslint-config-dicodingacademy';
+ 
+export default defineConfig([
+  daStyle,
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+
+]);
+
---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const { addNoteHandler, getALLNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+
+    {
+
+        method: 'GET',
+        path: '/notes',
+        handler: getALLNotesHandler,
+    },
+
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    }
+
+];
+
+module.exports = routes;
